054e7c09b1bf5bdd7ab7e4cbdc7de5df11861a02
mykim-multiply <mykim@m-ultiply.com>
2022-08-17 13:41:28 +0900

    eslint 셋팅

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "no-else-return": "error",
+    "default-param-last": "error",
+    "no-var": "error",
+    "no-lone-blocks": "error",
+    "require-await": "error",
+    "no-eq-null": "error",
+  },
+};
