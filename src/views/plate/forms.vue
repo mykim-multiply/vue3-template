5d51b9d0f12196a2783c584c15eb4a071cfdda80
mykim-multiply <mykim@m-ultiply.com>
2022-08-18 16:21:03 +0900

    vue3-cookies 추가

--- src/views/plate/forms.vue
+++ src/views/plate/forms.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>This is forms</div>
+  <q-page>
+    <button @click="saveCookie">쿠키저장</button>
+    <button @click="printCookie">쿠키콘솔</button>
+  </q-page>
 </template>
 
 <script>
+  import { useCookies } from "vue3-cookies";
+
+  const { cookies } = useCookies();
   export default {
     name: "PlateForms",
+    methods: {
+      saveCookie() {
+        cookies.set("test", "Hello?");
+      },
+      printCookie() {
+        const temp = cookies.get("test");
+        console.log("cookie > ", temp);
+      },
+    },
   };
 </script>
