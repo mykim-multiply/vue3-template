9f3c760a1db5a5ca7da9a106ed6560736c6b73dd
mykim-multiply <mykim@m-ultiply.com>
2022-08-18 17:24:27 +0900

    폰트스케일 시스템

--- src/views/plate/forms.vue
+++ src/views/plate/forms.vue
@@ -1,23 +1,34 @@
 <template>
   <q-page>
-    <button @click="saveCookie">쿠키저장</button>
-    <button @click="printCookie">쿠키콘솔</button>
+    <button @click="$q.dark.set(false)">라이트모드</button>
+    <button @click="$q.dark.set(true)">다크모드</button>
+    <br />
+    <div class="flex items-center">
+      <h5 class="text-h5 q-mr-lg" style="font-size: 16px">글씨배율 변경</h5>
+      <q-select v-model="fontScale" :options="fontScaleOptions" label="글씨 배율 설정" style="width: 200px" />
+    </div>
+    <p style="font-size: 1rem">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+      ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+      only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+      with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+      PageMaker including versions of Lorem Ipsum.
+    </p>
   </q-page>
 </template>
 
 <script>
-  import { useCookies } from "vue3-cookies";
-
-  const { cookies } = useCookies();
   export default {
     name: "PlateForms",
-    methods: {
-      saveCookie() {
-        cookies.set("test", "Hello?");
-      },
-      printCookie() {
-        const temp = cookies.get("test");
-        console.log("cookie > ", temp);
+    data() {
+      return {
+        fontScale: 1,
+        fontScaleOptions: [1, 2, 3, 4, 5],
+      };
+    },
+    watch: {
+      fontScale(val) {
+        document.documentElement.style.setProperty("--default-font-scale", val);
       },
     },
   };
